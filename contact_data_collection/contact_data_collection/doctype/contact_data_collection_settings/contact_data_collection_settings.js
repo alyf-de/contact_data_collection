e8e04714c5fd7e593bdf0c2a48f3721341790536
barredterra <14891507+barredterra@users.noreply.github.com>
2020-11-02 18:27:58 +0100

    feat: Contact Data Collection Settings

---
+++ contact_data_collection/contact_data_collection/doctype/contact_data_collection_settings/contact_data_collection_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, ALYF GmbH and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Contact Data Collection Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
